451ee0e31847f7921c4e564c179563e62d1c14b7
Luc <luc.viguie@gmail.com>
2020-11-02 16:44:41 +0100

    New lockdown

--- generate.js
+++ generate.js
@@ -4,9 +4,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         // Stay compatible with previous version search param
         data = window.location.search;
     }
-    console.log(data)
     const urlParams = new URLSearchParams(data);
-
     document.getElementById("field-firstname").value = urlParams.get("f"); //firstname
     document.getElementById("field-lastname").value = urlParams.get("l"); //lastname
     document.getElementById("field-birthday").value = urlParams.get("b"); //birthday
@@ -14,23 +12,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("field-address").value = urlParams.get("a"); //address
     document.getElementById("field-city").value = urlParams.get("c"); //city
     document.getElementById("field-zipcode").value = urlParams.get("z"); //zipcode
-    const reasons = urlParams.get("r"); //reason
-    //const reason = urlParams.get("r"); //reason
-    if(reasons.search('work') >= 0) document.getElementById("checkbox-travail").checked = true;
-    if(reasons.search('shopping') >=0) document.getElementById("checkbox-achats").checked = true;
-    if(reasons.search('health') >=0) document.getElementById("checkbox-sante").checked = true;
-    if(reasons.search('family') >=0) document.getElementById("checkbox-famille").checked = true;
-    if(reasons.search('disability') >=0) document.getElementById("checkbox-handicap").checked = true;
-    if(reasons.search('sport') >=0) document.getElementById("checkbox-sport_animaux").checked = true;
-    if(reasons.search('convocation') >=0) document.getElementById("checkbox-convocation").checked = true;
-    if(reasons.search('mission') >=0) document.getElementById("checkbox-missions").checked = true;
-    if(reasons.search('children') >=0) document.getElementById("checkbox-enfants").checked = true;
-    //document.getElementById("checkbox-travail").checked = (reason==='work');
-    //document.getElementById("checkbox-courses").checked = (reason==='buy');
-    //document.getElementById("checkbox-sante").checked = (reason==='health');
-    //document.getElementById("checkbox-famille").checked = (reason==='family');
-    //document.getElementById("checkbox-sport").checked = (reason==='sport');
-    //document.getElementById("checkbox-judiciaire").checked = (reason==='legal');
-    //document.getElementById("checkbox-missions").checked = (reason==='mission');
-    document.getElementById("generate-btn").click()
+
+    const reason = urlParams.get("r"); //reason
+    document.getElementById("radio-travail").checked = (reason==='travail');
+    document.getElementById("radio-achats").checked = (reason==='achats');
+    document.getElementById("radio-enfants").checked = (reason==='enfants');
+    document.getElementById("radio-sport_animaux").checked = (reason==='sport_animaux');
+    document.getElementById("radio-sante").checked = (reason==='sante');
+    document.getElementById("radio-famille").checked = (reason==='famille');
+    document.getElementById("radio-handicap").checked = (reason==='handicap');
+    document.getElementById("radio-convocation").checked = (reason==='convocation');
+    document.getElementById("radio-missions").checked = (reason==='missions');
+
+    var now = new Date()
+    document.getElementById('field-datesortie').value = now.toJSON().slice(0,10);
+    document.getElementById('field-heuresortie').value = now.toLocaleString("fr-FR", {timeZone: "Europe/Paris", hour: "2-digit", minute: "2-digit", hour12: false});
+    setTimeout(() => {
+        document.getElementById("generate-btn").click()
+    }, 500)
 });
+
+function badScriptLoading(event) {
+    console.log("Official JS is not working. Patching using local copy.")
+    let script = document.createElement('script');
+    script.src = "deplacement-covid-19/main.d56e3230.js";
+    document.head.append(script)
+}
